f71a69a5fd5c19b0030d5ec2996fee7891783e52
Ti Mo <timothei.dupire@viacesi.fr>
2017-01-26 11:06:24 +0100

    creation des pages linkNotExist et alreadyExist

---
+++ alreadyExist.php
@@ -0,0 +1,20 @@
+<!<!DOCTYPE>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Exist</title>
+        <link rel="stylesheet" type="text/css" href="">
+    </head>
+
+    <body>
+
+        <?php require("html/header.html"); ?>
+
+        <div>
+            <p>Message d'erreur: Les informations liées à cette addresse Email sont déjà existante,<br>
+            Pour plus de renseignement veuillez-nous contacter par mail au: <A HREF="mailto:mailing@connectlife.fr">mailing@connectlife.fr</A></p>
+        </div>
+
+
+    </body>
+</html>
---
+++ linkNotExist.php
@@ -0,0 +1,19 @@
+<!<!DOCTYPE>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>ERROR LINK</title>
+        <link rel="stylesheet" type="text/css" href="">
+    </head>
+
+    <body>
+
+        <?php require("html/header.html"); ?>
+
+        <div>
+            <p>Message d'erreur suite à un clic sur un lien hypertexte non existant</p>
+        </div>
+
+
+    </body>
+</html>
